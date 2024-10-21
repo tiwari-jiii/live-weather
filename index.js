a76a9784dfb4b72deb7b5d3b0d819fc338fb420c
tiwari-jiii <lakshyatiwari1805@gmail.com>
2024-10-21 12:03:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,17 @@ async function checkweather(city)
                         document.querySelector(".weather").style.display = "block";
 }
 
+
+searchBox.addEventListener("keydown", function(event) {
+
+  if (event.key === "Enter") {
+
+    event.preventDefault();
+ 
+    checkweather(searchBox.value);
+  }
+});
+
 searchBtn.addEventListener("click", ()=>{
 
     checkweather(searchBox.value);
